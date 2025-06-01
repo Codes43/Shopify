20ebb72556bad2bbfd3e13ea2b424116b555de60
Albert Waneloba <128598767+Codes43@users.noreply.github.com>
2025-06-01 10:20:02 +0300

    Update signuppage.dart
    Jst rebasing the project

--- lib/screens/signuppage.dart
+++ lib/screens/signuppage.dart
@@ -1,12 +1,211 @@
 // lib/screens/signup.dart
+// lib/screens/signup.dart
 import 'package:flutter/material.dart';
+import 'loginpage.dart';
+class SignupPage extends  StatefulWidget{
+  const SignupPage({super.key});
 
-class SignupPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  State<SignupPage> createState()=> _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignupPage>{
+
+  final _formKey=GlobalKey<FormState>();
+  final _emailController=TextEditingController();
+  final _passwordController=TextEditingController();
+  final _confirmPasswordController=TextEditingController();
+
+  bool _obscurePassword=true;
+  bool _obscureConfrimPassword=true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    bool isHoveringSignin=false;
     return Scaffold(
-      appBar: AppBar(title: Text('Signup')),
-      body: Center(child: Text('This is the Signup page')),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: const IconThemeData(color: Colors.black),
+        ),
+        body: SingleChildScrollView(
+            padding: const EdgeInsets.all(24.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Create Account',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+
+                  const SizedBox(height: 40),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: const TextStyle(color: Colors.black)
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(value)) {
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+
+                  ),
+
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                          color: Colors.black
+                      ),
+                      suffixIcon:
+                      IconButton(
+                        icon: Icon(
+                          _obscurePassword ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            _obscurePassword =!_obscurePassword;
+                          });
+
+                        },
+                      ),
+                    ),
+                    obscureText: _obscurePassword,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a password';
+                      }
+                      if (value.length < 6) {
+                        return 'Password must be at least 6 characters';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    decoration: InputDecoration(
+                      labelText: 'Confirm Password',
+                      labelStyle: const TextStyle(color: Colors.black),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureConfrimPassword ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.black,
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            _obscureConfrimPassword=!_obscureConfrimPassword;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: _obscureConfrimPassword,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your password';
+                      }
+                      if (value != _passwordController.text) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child:  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: (){
+                        if(_formKey.currentState!.validate()){
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data'))
+                          );
+                        }
+                      },
+                      child: const Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                      child:Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Already have an account?',
+                            style: TextStyle(fontWeight:FontWeight.bold),
+                          ),
+                          MouseRegion(
+                            onEnter: (_) => setState(() => isHoveringSignin = true),
+                            onExit: (_) => setState(() => isHoveringSignin = false),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (_) => LoginPage()),
+                                );
+                              },
+                              child: Text(
+                                'Signup',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.red,
+                                  decoration:
+                                  isHoveringSignin
+                                      ? TextDecoration.underline
+                                      : null,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+
+                  ),
+
+                ],
+              ),
+            )
+        )
     );
   }
 }
